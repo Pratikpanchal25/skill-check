71c6e297b7e8344ce167318133d5628076a98ba1
Pratik Panchal <pratikpanchal2505@gmail.com>
2026-01-13 12:37:45 +0530

    test-commit

--- server/database/db.ts
+++ server/database/db.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 export default function connectDB(): void {
     mongoose.connect(process.env.MONGO_URI as string, {
-        dbName: process.env.MONGO_DB_NAME || 'claimcraft'
+        dbName: process.env.MONGO_DB_NAME || 'skill_check'
     }).then(() => {
         console.log('MongoDB connected')
     }).catch((err) => {
